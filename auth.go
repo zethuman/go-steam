c7678faef81aa8d118fbad88319f393000130c2c
Rakhat Zhuman <zhumanrakhat01@gmail.com>
2023-10-21 09:31:27 +0600

    removed go.mod
    Signed-off-by: Rakhat Zhuman <zhumanrakhat01@gmail.com>

--- auth.go
+++ auth.go
@@ -5,6 +5,10 @@ import (
 	"sync/atomic"
 	"time"
 
+	. "github.com/faceit/go-steam/protocol"
+	. "github.com/faceit/go-steam/protocol/protobuf"
+	. "github.com/faceit/go-steam/protocol/steamlang"
+
 	"github.com/faceit/go-steam/steamid"
 	"github.com/golang/protobuf/proto"
 )
